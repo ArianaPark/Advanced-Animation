52ed13879b23df7e0b330cda9d3bb89681992ec5
ArianaPark <792913@seq.org>
2017-10-27 15:00:51 -0700

    5th commit-school

--- Lab1019/lab1019.js
+++ Lab1019/lab1019.js
@@ -3,10 +3,12 @@ window.onload = init; // Wait for the page to load before we begin animation
 var canvas;
 var ctx;// This is a better name for a global variable
 var imgCounter=0;
+var mushCount = 0;
 var numImgs = 12;
 var image;
 var animateDiv;
 var innerDiv;
+var mushDiv;
 
 
 function init(){
@@ -19,6 +21,12 @@ function init(){
   canvas.style.backgroundColor = 'rgba(255,255,255, 1)';
   // get the context
   ctx = canvas.getContext('2d'); // This is the context
+
+  mushDiv = document.getElementById("mushroomDiv");
+  mushDiv.style.width = "189px";
+  mushDiv.style.height = "230px";
+  mushDiv.style.border = "3px solid lightgreen";
+  mushDiv.style.backgroundImage = "url(mushroom.png)";
 
   animateDiv= document.getElementById("animationDiv");
   animateDiv.style.width = "200px";
@@ -34,6 +42,8 @@ function init(){
 
 function animate(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  //for manrunning
   var string = "man00";
   if(imgCounter<=9){
     string = string + "0";
@@ -54,4 +64,12 @@ function animate(){
   innerDiv.style.margin = ""+marginValueTop+"px "+marginValueRight+"px "+marginValueBottom+"px "+marginValueLeft+"px";
   innerDiv.style.backgroundPositionX = ""+(-source.x)+"px";
   innerDiv.style.backgroundPositionY = ""+(-source.y)+"px";
+
+  //for mushroom
+  if(mushCount===13) mushCount = 0;
+  var xpos = 189*(mushCount%5);
+  var ypos = 230*Math.floor((mushCount+1)/5);
+  mushCount++;
+  mushDiv.style.backgroundPositionX = ""+(-xpos)+"px";
+  mushDiv.style.backgroundPositionY = ""+(-ypos)+"px";
 }
